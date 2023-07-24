410538e9fc8bea0ca9f0348b8690e56b3d56eade
rahman majeed <rahmanmajeed46@gmail.com>
2023-07-24 22:55:55 +0600

    🌱 [intro] app entry

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  res.send("Heath check...");
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
