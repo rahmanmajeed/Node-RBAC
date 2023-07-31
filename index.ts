8d217e2b3fffadddb933da2a1d39c664b735635f
rahman majeed <rahmanmajeed46@gmail.com>
2023-07-31 21:55:46 +0600

    🧑‍💻 [apply] singleton pattern

--- index.ts
+++ index.ts
@@ -1,30 +1,9 @@
-import express, { Request, Response } from "express";
-import 'dotenv/config'
-import Server from "./src/core/Server";
+import "dotenv/config";
+import AppServer from "./src/core/AppServer";
 
-// Constants
-const PORT = 8080;
-const HOST = "0.0.0.0";
-
-// App
-// const app = express();
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("Hello World");
-// });
-
-// app.get("/health", (req: Request, res: Response) => {
-//   res.send("Heath check...");
-// });
-
-// app.listen(PORT, HOST, () => {
-//   console.log(`Running on http://${HOST}:${PORT}`);
-// });
-
-(async function(){
+(async function () {
   try {
-    let server = new Server();
-    await server.start()
-  } catch (error) {
-    
-  }
-})()
+    let server = AppServer.getAppInstance();
+    await server.start();
+  } catch (error) {}
+})();
---
+++ src/core/AppServer.ts
@@ -0,0 +1,51 @@
+import * as dotenv from "dotenv";
+import express, { Express } from "express";
+import { IServer } from "../interfaces/IServer";
+import BaseRouter from "../routes/base.route";
+dotenv.config();
+class AppServer implements IServer {
+  app: Express;
+
+  private static instance: AppServer;
+
+  private constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  public static getAppInstance(): AppServer {
+    if (!AppServer.instance) {
+      AppServer.instance = new AppServer();
+    }
+
+    return AppServer.instance;
+  }
+
+  config(): void {
+    this.app.set("port", process.env.APPLICATION_PUBLISH_PORT);
+    this.app.set("host", process.env.APPLICATION_HOST);
+    this.app.use("/", new BaseRouter().router);
+  }
+  server(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.app.listen(this.app.get("port"), this.app.get("host"), () => {
+        console.log(
+          `🟢⚡️[server]: Server is running at http://${this.app.get(
+            "host"
+          )}:${this.app.get("port")}`
+        );
+        resolve();
+      });
+    });
+  }
+
+  async start() {
+    // server start...
+    await this.server();
+    return {
+      express: this.app,
+    };
+  }
+}
+
+export default AppServer;
